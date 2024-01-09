c69be3507d237461df47f88794acc505115ee68e
djfksdl <djfksdl@gmail.com>
2024-01-09 18:11:21 +0900

    문제풀이중1

--- src/com/javaex/ex04/UserApp.java
+++ src/com/javaex/ex04/UserApp.java
@@ -4,7 +4,13 @@ public class UserApp {
 
 	public static void main(String[] args) {
 		
-
+		User[] uArray = new User[3];
+		
+		User u01 = new Customer("jws","j1234","정우성", 1000);
+		User u02 = new Customer("yjs","y2345","이효리",2000);
+		User u03 = new Employee("master","m7788","운영자",500);
+		
+		
 	}
 
 }
